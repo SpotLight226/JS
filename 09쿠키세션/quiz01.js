568d1a5ee2e46b398bc0fa11edacb1e9eb2cdffa
Seong Uk Jang <wkdqhfl7@gmail.com>
2023-07-03 23:53:12 +0900

    training

--- 09쿠키세션/quiz01.js
+++ 09쿠키세션/quiz01.js
@@ -1,9 +1,12 @@
 // img에서 세션생성 함수
-function createSesseion(target) {
-  var count = 0;
-  console.log(target);
-  localStorage.setItem('"' + count++ + '"', target);
-  if (localStorage.length === 3) {
-    localStorage.removeItem(localStorage.key(1));
+function setSession(target) {
+  // 길이가 1까지 ( 2개 까지 )는 세션에 저장하고
+  if (localStorage.length <= 1) {
+    console.log(target);
+    localStorage.setItem("img" + (localStorage.length + 1), target);
+    // 아니라면, 키1 에 키2의 값을, 키2에 새로 들어온 값을 저장
+  } else {
+    localStorage.setItem("img1", localStorage.getItem("img2"));
+    localStorage.setItem("img2", target);
   }
 }
